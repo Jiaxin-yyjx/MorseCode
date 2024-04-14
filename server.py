951f13f4412c016d56517dec1e7d2d575198d0b5
qf2172 <qf2172@columbia.edu>
2024-04-14 16:38:23 -0400

    intro & board

--- server.py
+++ server.py
@@ -14,6 +14,16 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     return render_template('home.html')
+@app.route('/intro')
+def intro():
+    return render_template('introduction.html')
+@app.route('/menuchar')
+def menuchar():
+    letters_row1 = [chr(i) for i in range(65, 75)]  # A-J
+    letters_row2 = [chr(i) for i in range(75, 84)]  # K-S
+    letters_row3 = [chr(i) for i in range(84, 91)]  # T-Z
+    words = ["SOS","HELLO","THANKS","LOVE","QRV"]
+    return render_template('menuchar.html', letters_row1=letters_row1, letters_row2=letters_row2, letters_row3=letters_row3, words=words)
 
 if __name__ == "__main__":
     app.run(debug=True)
