4f94d27ce0603930c739efe071f9bb61eeca714d
qf2172 <qf2172@columbia.edu>
2024-04-14 19:26:52 -0400

    add button

--- server.py
+++ server.py
@@ -6,6 +6,13 @@ from collections import UserString
 app = Flask(__name__)
 
 # Data
+words = {
+    0: "SOS",
+    1: "HELLO",
+    2: "THANKS",
+    3: "LOVE",
+    4: "QRV"
+}
 
 # Functions
 
@@ -22,11 +29,17 @@ def menuchar():
     letters_row1 = [chr(i) for i in range(65, 75)]  # A-J
     letters_row2 = [chr(i) for i in range(75, 84)]  # K-S
     letters_row3 = [chr(i) for i in range(84, 91)]  # T-Z
-    words = ["SOS","HELLO","THANKS","LOVE","QRV"]
     return render_template('menuchar.html', letters_row1=letters_row1, letters_row2=letters_row2, letters_row3=letters_row3, words=words)
 
-@app.route('/wordlearning/<word>')
-def word_learning(word):
-    return render_template('wordlearning.html', word=word)
+@app.route('/wordlearning/<int:index>')
+def word_learning(index):
+    return render_template('wordlearning.html', index=index, words=words, word=words[index])
+@app.route('/finishedlearning')
+def finished_learning():
+    return render_template('finishedlearning.html')
+@app.route('/start')
+def start():
+    return render_template('start.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
