a3bf9ea73ea0b71816fc8b559aaf7d988030e4b9
qf2172 <qf2172@columbia.edu>
2024-04-14 23:17:21 -0400

    need dashtree

--- server.py
+++ server.py
@@ -13,7 +13,6 @@ words = {
     3: "LOVE",
     4: "QRV"
 }
-
 # Functions
 
 
@@ -21,25 +20,29 @@ words = {
 @app.route('/')
 def homepage():
     return render_template('home.html')
+
 @app.route('/intro')
 def intro():
     return render_template('introduction.html')
+
 @app.route('/menuchar')
 def menuchar():
     letters_row1 = [chr(i) for i in range(65, 75)]  # A-J
     letters_row2 = [chr(i) for i in range(75, 84)]  # K-S
     letters_row3 = [chr(i) for i in range(84, 91)]  # T-Z
-    return render_template('menuchar.html', letters_row1=letters_row1, letters_row2=letters_row2, letters_row3=letters_row3, words=words)
+    return render_template('learn/menuchar.html', letters_row1=letters_row1, letters_row2=letters_row2, letters_row3=letters_row3, words=words)
 
 @app.route('/wordlearning/<int:index>')
 def word_learning(index):
-    return render_template('wordlearning.html', index=index, words=words, word=words[index])
+    return render_template('learn/wordlearning.html', index=index, words=words, word=words[index])
+
 @app.route('/finishedlearning')
 def finished_learning():
-    return render_template('finishedlearning.html')
+    return render_template('learn/finishedlearning.html')
+
 @app.route('/start')
 def start():
-    return render_template('start.html')
+    return render_template('learn/start.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
