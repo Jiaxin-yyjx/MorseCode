1cb7367b0f7dd533894ef933e4d8d4f7b20ddd03
qf2172 <qf2172@columbia.edu>
2024-05-03 12:32:08 -0400

    qfxxx

--- static/recordtime.js
+++ static/recordtime.js
@@ -1,25 +1,35 @@
 $(document).ready(function() {
     var dataContainer = document.getElementById('js-data');
-    var pageIndex = parseInt(dataContainer.getAttribute('data-index'), 10);
-    sessionStorage.setItem('startTime', Date.now());
-    $(window).on('beforeunload', function() {
-        var startTime = sessionStorage.getItem('startTime');
-        if (startTime) {
-            var endTime = Date.now();
-            var timeSpent = (endTime - startTime) / 1000;
-            console.log('Time spent on page index ' + pageIndex + ': ' + timeSpent + ' seconds');
-            $.ajax({
-                url: '/record_time',
-                type: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify({ timeSpent: timeSpent, index: pageIndex }),
-                success: function(response) {
-                    console.log('Time recorded successfully');
-                },
-                error: function(xhr, status, error) {
-                    console.log('Error recording time: ' + error);
-                }
-            });
-        }
-    });
+    console.log(dataContainer);
+
+    if (dataContainer) {
+        var pageIndex = parseInt(dataContainer.getAttribute('data-index'), 10);
+        sessionStorage.setItem('startTime', Date.now());
+        console.log(pageIndex);
+
+        $(window).on('beforeunload', function() {
+            var startTime = sessionStorage.getItem('startTime');
+            console.log(startTime);
+            if (startTime) {
+                var endTime = Date.now();
+                var timeSpent = (endTime - startTime) / 1000;
+                console.log('Time spent on page index ' + pageIndex + ': ' + timeSpent + ' seconds');
+
+                $.ajax({
+                    url: '/record_time',
+                    type: 'POST',
+                    contentType: 'application/json',
+                    data: JSON.stringify({ timeSpent: timeSpent, index: pageIndex }),
+                    success: function(response) {
+                        console.log('Time recorded successfully');
+                    },
+                    error: function(xhr, status, error) {
+                        console.log('Error recording time: ' + error);
+                    }
+                });
+            }
+        });
+    } else {
+        console.log('Error: js-data element not found on this page');
+    }
 });
